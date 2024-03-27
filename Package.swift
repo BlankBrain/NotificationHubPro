9fdeaac89d2294621aba2a52b0b54d368f78c0bc
Md. Mehedi Hasan <mehedihasan@ewnbd.com>
2024-03-27 22:56:42 +0600

    update 1

--- Package.swift
+++ Package.swift
@@ -2,27 +2,34 @@
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 
+
+
 import PackageDescription
 
 let package = Package(
     name: "NotificationHub",
-    platforms: [.iOS(.v14)],
+    platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "NotificationHub",
             targets: ["NotificationHub"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/BlankBrain/UtilityHub.git", from: Version(1, 0, 0)),
+        .package(url: "https://github.com/TheRakiburKhan/RKAPIService.git", from: Version(3, 0, 0)),
+        .package(url: "https://github.com/airbnb/lottie-ios.git", from: Version(4,0,0)),
+        .package(url: "https://github.com/SVGKit/SVGKit.git", from: Version(3, 0, 0)),
+        .package(url: "https://github.com/EFPrefix/EFQRCode.git", .upToNextMinor(from: "6.2.2"))
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "NotificationHub",
-            dependencies: ["UtilityHub"],
-            resources: [.process("Resources")])
+            dependencies: [.product(name: "Lottie", package: "lottie-ios"), "SVGKit", "RKAPIService" , "EFQRCode"]),
+        .testTarget(
+            name: "NotificationHubTests",
+            dependencies: ["NotificationHub"]),
     ]
 )
